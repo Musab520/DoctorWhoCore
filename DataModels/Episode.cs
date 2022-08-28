28321f43b32f5122017c55c532e0660fd7bcb6eb
Musab Abuasi <abuasimusab@gmail.com>
2022-08-29 00:40:07 +0300

    Refactor Models

---
+++ DataModels/Episode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorWho.Db
+{
+    public class Episode
+    {
+        public int EpisodeId { get; set; }
+        public int SeriesNumber { get; set; }
+        public int EpisodeNumber { get; set; }
+        public string EpisodeType { get; set; }
+        public string Title { get; set; }
+        public DateTime EpisodeDate { get; set; }
+        public Author author { get; set; }
+        public Doctor doctor { get; set; }
+        public int AuthorId { get; set; }
+        public int DoctorId { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ DataModels/EpisodeEnemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorWho.Db
+{
+    public class EpisodeEnemy
+    {
+        public int EpisodeEnemyId { get; set;}
+        public Episode episode { get; set; }
+        public Enemy enemy { get; set; }
+        public int EpisodeId { get; set; }
+        public int EnemyId { get; set; }
+    }
+}
