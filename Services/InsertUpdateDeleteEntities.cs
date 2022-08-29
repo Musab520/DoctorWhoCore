10cd2c16555378d3e8baf5fbdfa64f87b4154db3
Musab Abuasi <abuasimusab@gmail.com>
2022-08-29 16:11:03 +0300

    Added InsertUpdateDelete Methods

---
+++ Services/InsertUpdateDeleteEntities.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorWho.Db.Services
+{
+    public class InsertUpdateDeleteEntities
+    {
+        public void Insert<Entity>(DoctorWhoCoreDbContext context,Entity entity)
+        {
+            if (entity != null)
+            {
+                context.Add(entity);
+                context.SaveChanges();
+            }
+        }
+        public void Update<Entity>(DoctorWhoCoreDbContext context, Entity entity)
+        {
+            if (entity != null)
+            {
+                context.Update(entity);
+                context.SaveChanges();
+            }
+        }
+        public void Delete<Entity>(DoctorWhoCoreDbContext context, Entity entity)
+        {
+            if (entity != null)
+            {
+                context.Remove(entity);
+                context.SaveChanges();
+            }
+        }
+    }
+}
